e2b94383e6c2aced6d2cf1e8a16093502fafd5d2
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-25 21:32:41 +0100

    database ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './logochuj.png';
 import './App.css';
+import firebaseConfig from "./config"
+import Firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        Firebase.initializeApp(firebaseConfig);
+        const db = Firebase.firestore();
+
+        const myPost = db.collection('zwierzeta').doc('treWChZPgTjothNmvUN9');
+        myPost.onSnapshot(doc => {
+            const data = doc.data();
+            console.log(data)
+        });
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+            </div>
+        );
+    }
+
 }
 
 export default App;
