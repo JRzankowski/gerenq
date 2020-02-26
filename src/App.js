73a0cea1074f4d79d45a580bc7701e891b0b53f4
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-26 20:45:00 +0100

    structure ready

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, {Component} from 'react';
-import logo from './logochuj.png';
-import './App.css';
-import firebaseConfig from "./config"
-import Firebase from "firebase";
 
-class App extends Component {
+import Main from "./compontents/Main";
 
+export default class App extends Component {
     render() {
-        Firebase.initializeApp(firebaseConfig);
-        const db = Firebase.firestore();
-
-        const myPost = db.collection('zwierzeta').doc('treWChZPgTjothNmvUN9');
-        myPost.onSnapshot(doc => {
-            const data = doc.data();
-            console.log(data)
-        });
-
+        // Firebase.initializeApp(firebaseConfig);
+        // const db = Firebase.firestore();
+        // const myPost = db.collection('zwierzeta').doc('treWChZPgTjothNmvUN9');
+        // myPost.onSnapshot(doc => {
+        //     const data = doc.data();
+        //     console.log(data)
+        // });
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </header>
+          <Main/>
             </div>
         );
     }
 
 }
 
-export default App;
---
+++ src/compontents/Nav.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+export default class Nav extends Component {
+    render() {
+        return (
+            <div className="bg-toggle">
+                <nav className='nav'>
+
+                </nav>
+            </div>
+        );
+    }
+
+}
+
